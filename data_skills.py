a941adfac1356bb83caca22f4aec8a8355f4ac21
Bin Xuan Kong <binxuankong@gmail.com>
2021-09-28 14:52:16 +0800

    Scrape youtube videos based on data skills

---
+++ data_skills.py
@@ -0,0 +1,13 @@
+SKILLS = ['APACHE', 'C++', 'Financial Statements', 'MongoDB', 'R', 'Python', 'Java', 'Excel', 'Tableau', 'Statistics',
+          'Natural Language Processing (NLP)', 'Unsupervised Machine Learning', 'Structured Query Language (SQL)',
+          'Computer Vision', 'Supervised Machine Learning', 'Matlab', 'Extract Transform Load (ETL)', 'Deep Learning',
+          'Dashboard', 'NOSQL', 'SPARK', 'Artificial Intelligence (AI)', 'Time Series Analysis', 'Power BI',
+          'Regressions', 'Reinforcement', 'Graph Theory', 'Database Management System (DBMS)', 'Hadoop',
+          'Cloud Technologies', 'Data Cleansing', 'Data Preparation', 'Github', 'JavaScript', 'Presentation',
+          'AB Testing', 'Visualizations']
+
+SKILL_DICT = {'MongoDB': 'Mongo DB', 'R': 'R programming', 'Python': 'Python Programming',
+              'Natural Language Processing (NLP)': 'NLP', 'Structured Query Language (SQL)': 'SQL', 
+              'Extract Transform Load (ETL)': 'ETL', 'Dashboard': 'Dashboards', 'Artificial Intelligence (AI)': 'AI',
+              'Database Management System (DBMS)': 'DBMS', 'Data Cleansing': 'Data Cleansing / Preparation',
+              'Data Preparation': 'Data Cleansing / Preparation', 'Presentation': 'Presentation Skill'}
